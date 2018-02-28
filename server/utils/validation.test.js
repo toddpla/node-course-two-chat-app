524a5f1e97a415398acced899649487c94403667
toddpla <toddpla@gmail.com>
2018-02-28 23:54:55 +0000

    =Add users class and update user list event

---
+++ server/utils/validation.test.js
@@ -0,0 +1,19 @@
+const expect = require('expect');
+const {isRealString} = require('./validation')
+
+describe('isRealString',() => {
+  it('should reject non-string values', () => {
+    var res = isRealString(123);
+    expect(res).toBe(false);
+  });
+
+  it('should reject string with only spaces', () => {
+    var res = isRealString('      ');
+    expect(res).toBe(false);
+  });
+
+  it('should allow string with non-space characters', () => {
+    var res = isRealString('Todd');
+    expect(res).toBe(true);
+  });
+});
