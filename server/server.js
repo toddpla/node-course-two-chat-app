bcf3ecf5523e479925ec53691a63e61cc833765a
toddpla <toddpla@gmail.com>
2018-02-28 17:20:05 +0000

    =Add mustache.js for message templates

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ io.on('connection', (socket) => {
 
 socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app.'));
 
-  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined.'));
+socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined.'));
 
 socket.on('createMessage', (message, callback) => {
   console.log('createMessage', message);
